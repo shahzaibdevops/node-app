6157f90976ae807a4c4fb07ec9868891ba23020f
shahzaibdevops <54997985+shahzaibdevops@users.noreply.github.com>
2019-12-18 12:35:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
+// Appp
 const app = express();
 app.get('/', (req, res) => {
   res.send('<h1 style="color:red;">Java Home App - version-8!!</h1> \n');
